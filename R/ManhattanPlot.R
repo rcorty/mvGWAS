29e667d8ae4080a309afcc1821d86cc44f964399
Robert Corty <rcorty@gmail.com>
2017-06-05 16:24:09 -0400

    minor updates
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/ManhattanPlot.R
+++ R/ManhattanPlot.R
@@ -2,8 +2,27 @@
 #' @name ManhattanPlot
 #' @author Robert Corty
 #'
-#' @param df
-#' @param ...
+#' @param df The tbl_df to be plotted.  Must have columns called 'chr', 'pos',
+#' 'mean.p', and 'var.p'.
+#' @param lower.bound The lowest value to be plotted.  This number will be
+#' interpreted on the negative log ten scale, so 2 implies that the least
+#' significant p-value to be plotted will be 0.01.
+#' @param hit.cutoff The lowest value that gets a bigger dot, drawing
+#' attention to it.  Default to \code{-log10(0.05/nrow(df))}.
+#' @param hit.alpha Alpha value (opacity) for SNPs above the hit cutoff.
+#' Defaults to 0.8
+#' @param non.hit.alpha Alpha value (opacity) for SNPs below the hit cutoff.
+#' Defaults to 0.3.
+#' @param hit.point.size Size of points above the hit cutoff.  Defaults
+#' to 1.
+#' @param non.hit.point.size Size of poitns below the hit cutoff.  Defaults
+#' to 0.3.
+#' @param do.neg.log.ten Whether this ploting function should compute the
+#' -log10 of the p-values supplied in \code{df}.  Defaults to TRUE, which
+#' is valid when p-values are supplied.  -log10 of p-values may be provided
+#' in some cases, e.g. when using this plotting function in the context of
+#' a Shiny app that uses brushing to select a SNP.
+#' @param use.plotly
 #'
 #' @return
 #' @export
@@ -11,19 +30,23 @@
 #' @examples
 #'
 ManhattanPlot <- function(df,
-                          use.plotly = FALSE,
                           lower.bound = 2,
-                          hit.cutoff = 4,
+                          hit.cutoff = -log10(0.05/nrow(df)),
                           hit.alpha = 0.8,
                           non.hit.alpha = 0.3,
                           hit.point.size = 1,
                           non.hit.point.size = 0.5,
-                          do.neg.log.ten = TRUE) {
+                          do.neg.log.ten = TRUE,
+                          use.plotly = FALSE) {
 
-  if (!all(c('chr', 'pos', 'mean.p', 'var.p') %in% names(df))) {
-    stop("tbl_df to make Manhattan Plot must contain columns 'chr', 'pos', 'mean.p', and 'var.p'")
-  }
+  # Make sure all the right columns are present
+  # May want to also check their types
+  stopifnot(all(c('chr', 'pos', 'mean.p', 'var.p') %in% names(df)))
 
+  # Make sure no p-values are infinite
+  stopifnot(!any(is.infinite(df$mean.p), is.infinite(df$var.p)))
+
+  # switch to neg log ten scale if needed
   if (do.neg.log.ten) {
     df$mean.p <- -log10(df$mean.p)
     df$var.p <- -log10(df$var.p)
@@ -35,7 +58,7 @@ ManhattanPlot <- function(df,
     geom_point(data = subset(df, mean.p > hit.cutoff & !is.na(mean.p)), mapping = aes(y = mean.p), color = 'blue', size = hit.point.size, shape = 21, alpha = hit.alpha) +
     geom_point(data = subset(df, var.p > lower.bound & !is.na(var.p)), mapping = aes(y = var.p), color = 'red', size = non.hit.point.size, alpha = non.hit.alpha) +
     geom_point(data = subset(df, var.p > hit.cutoff & !is.na(var.p)), mapping = aes(y = var.p), color = 'red', size = hit.point.size, shape = 21, alpha = hit.alpha) +
-    scale_y_continuous(expand = c(0.01, 0)) +
+    scale_y_continuous(expand = c(0.02, 0)) +
     ggthemes::theme_hc() +
     theme(axis.ticks.x = element_blank(),
           axis.text.x = element_blank()) +
