7c55e34c86605dba906c052c6d73ddfdcbf8de86
Robert Corty <rcorty@gmail.com>
2017-06-29 17:54:30 -0400

    shuffled functions so it builds without error
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/conduct_scan.R
+++ R/conduct_scan.R
@@ -1,148 +1,3 @@
-#' @title conduct_scan
-#' @name mvGWAS_conduct_scan
-#' @include mvGWAS.R
-#'
-#' @param mean_formula the mean formula
-#' @param var_formula the variance formula
-#' @param computer Either 'local' (default) or 'slurm'. Should the scan be conducted on the local machine or on a slurm cluster.
-#'
-#' @description conducts an mvGWAS
-#'
-#' @return the mvGWAS object  (change this?)
-#'
-mvGWAS$methods(
-  conduct_scan = function(mean_formula,
-                          var_formula,
-                          system = c('local', 'slurm'),
-                          ...) {
-
-    system <- match.arg(arg = system)
-
-    determine_keyword_use_(mean_formula, var_formula)
-
-    stash_formula_(formula = mean_formula, type = 'mean')
-    stash_formula_(formula = var_formula, type = 'var')
-
-    null_model <<- dglm::dglm(formula = metadata$mean_null_formula,
-                              dformula = metadata$var_null_formula,
-                              data = data$phenotypes)
-
-    if (length(metadata$used_keywords) == 0) {
-      message('No keywords in mean_formula nor var_formula.  Returning without scanny any SNPs.')
-      return(TRUE)
-    }
-
-    switch(EXPR = system,
-           'local' = conduct_scan_local_(...),
-           'slurm' = conduct_scan_slurm_(...))
-
-    results$CHROM <<- factor(x = results$CHROM,
-                             levels = stringr::str_pad(string = unique(results$CHROM), width = 2, pad = '0'))
-
-    return(TRUE)
-
-  }
-)
-
-
-
-
-#' @title determine_keyword_use_
-#' @name mvGWAS_internals
-#'
-#' @description determines which keywords are used in mean_formula and var_formula
-#'
-#' @return nothing
-#'
-mvGWAS$methods(
-  determine_keyword_use_ = function(mean_formula, var_formula) {
-
-    used_keyword_idxs <- metadata$available_keywords %in% c(all.vars(mean_formula), all.vars(var_formula))
-
-    metadata$used_keywords <<- metadata$available_keywords[used_keyword_idxs]
-
-  }
-)
-
-
-#' @title stash_formula_
-#' @name mvGWAS_internals
-#'
-#' @param formula the formula to validate
-#' @param type one of 'mean' or 'var'
-#'
-#' @description validates a formula and stashes it for use in an mvGWAS
-#'
-#' @return nothing
-#'
-mvGWAS$methods(
-  stash_formula_ = function(formula, type = c('mean', 'var')) {
-
-    type <- match.arg(arg = type)
-
-    stopifnot(inherits(x = formula, 'formula'))
-    stopifnot(length(formula) == switch(EXPR = type, mean = 3, var = 2))
-    stopifnot(all.vars(expr = formula) %in% c(names(data$phenotypes), metadata$available_keywords))
-
-    # derive and stash null formula
-    rhs <- formula[[switch(EXPR = type, mean = 3, var = 2)]]
-    rhs_vars <- all.vars(rhs)
-    keyword_idxs <- grep(pattern = paste(metadata$available_keywords, collapse = '|'), x = rhs_vars)
-
-    # If the formula uses one or more keywords (the words used to refer to the locus),
-    # remove them to create a null formula then save the original as the alternative formula
-    if (any(keyword_idxs)) {
-
-      # corner case where there are no non-keyword RHS terms
-      if (length(keyword_idxs) == length(rhs_vars)) {
-
-        r <- switch(EXPR = type, mean = formula[[2]], var = NULL)
-        null_formula <- stats::reformulate(termlabels = '1', response = r)
-
-        # typical case
-      } else {
-
-        kr <- switch(EXPR = type, mean = TRUE, var = FALSE)
-        keyword_free <- stats::drop.terms(termobj = stats::terms(formula),
-                                          dropx = keyword_idxs,
-                                          keep.response = kr)
-        null_formula <- stats::formula(x = keyword_free)
-
-      }
-
-      # modify formula if it uses GP -- need two terms -- one for het and one for homozygous alternative
-      if ('GP' %in% all.vars(expr = formula)) {
-
-        r <- switch(EXPR = type, mean = formula[[2]], var = NULL)
-        new_terms <- gsub(pattern = 'GP', replacement = 'GP_het + GP_alt', x = labels(stats::terms(formula)))
-        alt_formula <- reformulate(termlabels = new_terms, response = r)
-
-      } else {
-        alt_formula <- formula
-      }
-
-      # If the formula doesn't use any keywords, just save it as the null and we don't have an alt
-    } else {
-
-      alt_formula <- NULL
-      null_formula <- formula
-
-    }
-
-    metadata[[paste0(type, '_null_formula')]] <<- null_formula
-    metadata[[paste0(type, '_alt_formula')]] <<- alt_formula
-
-
-
-
-
-
-  }
-)
-
-
-
-
 #' @title scan_vcf_file_
 #' @name mvGWAS_internals
 #'
@@ -378,3 +233,149 @@ mvGWAS$methods(
 )
 
 
+
+#' @title conduct_scan
+#' @name mvGWAS_conduct_scan
+#' @include mvGWAS.R
+#'
+#' @param mean_formula the mean formula
+#' @param var_formula the variance formula
+#' @param computer Either 'local' (default) or 'slurm'. Should the scan be conducted on the local machine or on a slurm cluster.
+#'
+#' @description conducts an mvGWAS
+#'
+#' @return the mvGWAS object  (change this?)
+#'
+mvGWAS$methods(
+  conduct_scan = function(mean_formula,
+                          var_formula,
+                          system = c('local', 'slurm'),
+                          ...) {
+
+    system <- match.arg(arg = system)
+
+    determine_keyword_use_(mean_formula, var_formula)
+
+    stash_formula_(formula = mean_formula, type = 'mean')
+    stash_formula_(formula = var_formula, type = 'var')
+
+    null_model <<- dglm::dglm(formula = metadata$mean_null_formula,
+                              dformula = metadata$var_null_formula,
+                              data = data$phenotypes)
+
+    if (length(metadata$used_keywords) == 0) {
+      message('No keywords in mean_formula nor var_formula.  Returning without scanny any SNPs.')
+      return(TRUE)
+    }
+
+    switch(EXPR = system,
+           'local' = conduct_scan_local_(...),
+           'slurm' = conduct_scan_slurm_(...))
+
+    results <<- results %>%
+      dplyr::mutate(CHROM = factor(CHROM,
+                                   levels = stringr::str_pad(string = unique(results$CHROM), width = 2, pad = '0')))
+
+    return(TRUE)
+
+  }
+)
+
+
+
+
+
+
+
+#' @title determine_keyword_use_
+#' @name mvGWAS_internals
+#'
+#' @description determines which keywords are used in mean_formula and var_formula
+#'
+#' @return nothing
+#'
+mvGWAS$methods(
+  determine_keyword_use_ = function(mean_formula, var_formula) {
+
+    used_keyword_idxs <- metadata$available_keywords %in% c(all.vars(mean_formula), all.vars(var_formula))
+
+    metadata$used_keywords <<- metadata$available_keywords[used_keyword_idxs]
+
+  }
+)
+
+
+#' @title stash_formula_
+#' @name mvGWAS_internals
+#'
+#' @param formula the formula to validate
+#' @param type one of 'mean' or 'var'
+#'
+#' @description validates a formula and stashes it for use in an mvGWAS
+#'
+#' @return nothing
+#'
+mvGWAS$methods(
+  stash_formula_ = function(formula, type = c('mean', 'var')) {
+
+    type <- match.arg(arg = type)
+
+    stopifnot(inherits(x = formula, 'formula'))
+    stopifnot(length(formula) == switch(EXPR = type, mean = 3, var = 2))
+    stopifnot(all.vars(expr = formula) %in% c(names(data$phenotypes), metadata$available_keywords))
+
+    # derive and stash null formula
+    rhs <- formula[[switch(EXPR = type, mean = 3, var = 2)]]
+    rhs_vars <- all.vars(rhs)
+    keyword_idxs <- grep(pattern = paste(metadata$available_keywords, collapse = '|'), x = rhs_vars)
+
+    # If the formula uses one or more keywords (the words used to refer to the locus),
+    # remove them to create a null formula then save the original as the alternative formula
+    if (any(keyword_idxs)) {
+
+      # corner case where there are no non-keyword RHS terms
+      if (length(keyword_idxs) == length(rhs_vars)) {
+
+        r <- switch(EXPR = type, mean = formula[[2]], var = NULL)
+        null_formula <- stats::reformulate(termlabels = '1', response = r)
+
+        # typical case
+      } else {
+
+        kr <- switch(EXPR = type, mean = TRUE, var = FALSE)
+        keyword_free <- stats::drop.terms(termobj = stats::terms(formula),
+                                          dropx = keyword_idxs,
+                                          keep.response = kr)
+        null_formula <- stats::formula(x = keyword_free)
+
+      }
+
+      # modify formula if it uses GP -- need two terms -- one for het and one for homozygous alternative
+      if ('GP' %in% all.vars(expr = formula)) {
+
+        r <- switch(EXPR = type, mean = formula[[2]], var = NULL)
+        new_terms <- gsub(pattern = 'GP', replacement = 'GP_het + GP_alt', x = labels(stats::terms(formula)))
+        alt_formula <- reformulate(termlabels = new_terms, response = r)
+
+      } else {
+        alt_formula <- formula
+      }
+
+      # If the formula doesn't use any keywords, just save it as the null and we don't have an alt
+    } else {
+
+      alt_formula <- NULL
+      null_formula <- formula
+
+    }
+
+    metadata[[paste0(type, '_null_formula')]] <<- null_formula
+    metadata[[paste0(type, '_alt_formula')]] <<- alt_formula
+
+
+
+
+
+
+  }
+)
