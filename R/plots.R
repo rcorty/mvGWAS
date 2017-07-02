bedc2ce53ad197c51f41a9f7ccb3cd00da5f7aaf
Robert Corty <rcorty@gmail.com>
2017-07-01 23:29:24 -0400

    improved manhattan_plot()
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/plots.R
+++ R/plots.R
@@ -1,46 +1,51 @@
-#' @title manhattan_plot
+#' @title Manhattan Plot
 #' @name mvGWAS_manhattan_plot
-#' @include mvGWAS.R
 #'
-#' @description makes a Manhattan plot
+#' @description makes a manhattan plot for a mvGWAS
+#'
+#' @param what_to_plot what to plot
+#' @param lowest_nlog_p_to_plot lowest neg log10 p to plot
+#' @param highlight_nlog_p_above highlight neg log10 p value above this number with larger points
+#' @param highlight_size how big are the highlight points
+#' @param nonhighlight_alpha how transparent are the non-highlighted points
 #'
 #' @return the plot
 #' @importFrom dplyr %>%
 #'
 mvGWAS$methods(
-  manhattan_plot = function(what_to_plot = c('asymp_p', 'asymp_p_gc'),
+
+  manhattan_plot = function(what = c('p_LR', 'p_z_DS'),
+                            gc,
                             lowest_nlog_p_to_plot = 3,
                             highlight_nlog_p_above = 5,
                             highlight_size = 2,
                             nonhighlight_alpha = 0.5) {
 
-    what_to_plot <- match.arg(arg = what_to_plot)
+    what <- match.arg(arg = what)
+    if (missing(gc)) { gc <- any(grepl(pattern = paste0(what, '.*_gc$'), x = names(results)))}
 
-    if (what_to_plot == 'asymp_p') {
+    column_regex <- paste0('^', what, '.*', if (gc) '_gc$')
 
-      for_plotting <- results %>%
-        tidyr::gather(key = test, value = p, mean_asymp_p, var_asymp_p, joint_asymp_p) %>%
-        dplyr::mutate(test = dplyr::recode(test, mean_asymp_p = 'mean', var_asymp_p = 'var', joint_asymp_p = 'joint'))
+    to_plot <- gwas1$results %>%
+      dplyr::select(matches(column_regex), CHROM, POS) %>%
+      tidyr::gather(key = test, value = p, matches(column_regex)) %>%
+      na.omit() %>%
+      dplyr::mutate(nlog_p = -log(p, base = 10))
+    # dplyr::mutate(test = dplyr::recode(test, mean_asymp_p = 'mean', var_asymp_p = 'var', joint_asymp_p = 'joint'))
 
-    } else {
 
-      stop('mahnattan_plot() on gc values not yet implemented')
-
-    }
-
-    for_plotting2 <- for_plotting %>%
-      dplyr::mutate(nlog_p = -log(p, base = 10)) %>%
+    to_plot2 <- to_plot %>%
       dplyr::filter(nlog_p > lowest_nlog_p_to_plot, nlog_p < highlight_nlog_p_above)
 
-    for_plotting3 <- for_plotting %>%
-      dplyr::mutate(nlog_p = -log(p, base = 10)) %>%
+    to_plot3 <- to_plot %>%
       dplyr::filter(nlog_p > highlight_nlog_p_above)
 
-    ggplot2::ggplot(data = for_plotting2, mapping = ggplot2::aes(x = POS, y = nlog_p, color = test)) +
+    ggplot2::ggplot(data = to_plot2, mapping = ggplot2::aes(x = POS, y = nlog_p, color = test)) +
       ggplot2::facet_grid(~CHROM, scales = 'free_x', switch = 'x', space = 'free', drop = FALSE) +
       ggplot2::geom_point(alpha = nonhighlight_alpha) +
-      ggplot2::geom_point(data = for_plotting3, size = highlight_size) +
-      ggplot2::scale_color_manual(values = c(mean = 'blue', var = 'red', joint = 'black')) +
+      ggplot2::geom_point(data = to_plot3, size = highlight_size) +
+      ggplot2::scale_color_manual(values = setNames(object = c('blue', 'red', 'black'),
+                                                    nm = paste0(what, '_', c('mean', 'var', 'joint'), if (gc) '_gc'))) +
       ggplot2::theme_minimal() +
       ggplot2::theme(axis.ticks.x = ggplot2::element_blank(),
                      axis.text.x = ggplot2::element_blank(),
