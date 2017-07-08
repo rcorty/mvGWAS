88f64bc1a376969251a3d6d2208fdd7cf363758d
Robert Corty <rcorty@gmail.com>
2017-07-08 16:36:56 -0400

    nada
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- sandbox.R
+++ sandbox.R
@@ -1,32 +1,26 @@
 library(dplyr)
 library(mvGWAS)
 
-gwas1 <- mvGWAS$new(phenotype_file = 'tests/test_data/test_phenos.RDS', genotype_directory = 'tests/test_data')
+my_gwas <- mvGWAS$new(phenotype_file = 'tests/test_data/test_phenos.RDS', genotype_directory = 'tests/test_data')
 
-gwas1$conduct_scan(mean_formula = sbp ~ EV1 + male + DS, var_formula = ~ EV1 + male + DS)
+my_gwas$conduct_scan(mean_formula = sbp ~ EV1 + male + DS, var_formula = ~ EV1 + male + GT, num_cores = 1)
 
-gwas1$qq_plot(what = 'p_LR')
-gwas1$manhattan_plot(what = 'p_LR', lowest_nlog_p_to_plot = 2)
+my_gwas$qq_plot(what = 'p_LR')
+my_gwas$manhattan_plot(what = 'p_LR', lowest_nlog_p_to_plot = 1)
 
-gwas1$apply_genomic_control(keyword = 'LR')
-gwas1$genomic_control_dfs
-gwas1$results %>% glimpse()
-
-
-gwas1$qq_plot(what = 'p_LR')
-gwas1$manhattan_plot(what = 'p_LR', lowest_nlog_p_to_plot = 2)
+# gwas1$apply_genomic_control(method = 'LR')
+# gwas1$genomic_control_dfs
+# gwas1$results %>% glimpse()
+#
+#
+# gwas1$qq_plot(what = 'p_LR')
+# gwas1$manhattan_plot(what = 'p_LR', lowest_nlog_p_to_plot = 2)
 
 
 
 gwas1$qq_plot(what = 'p_z_DS')
 gwas1$manhattan_plot(what = 'p_z_DS', lowest_nlog_p_to_plot = 1)
 
-gwas1$apply_genomic_control(keyword = 'z_DS')
+gwas1$apply_genomic_control(method = 'z2_DS')
 
 
-
-# gwas1$apply_genomic_control(keyword = 'DS')
-# gwas1$genomic_control_dfs
-# gwas1$results %>% glimpse()
-#
-#
