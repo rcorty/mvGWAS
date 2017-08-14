e4ea775ae7e8f3d9d67f1e1c441e335aa944fedd
Robert Corty <rcorty@gmail.com>
2017-08-14 15:05:46 -0400

    added dglm_norm function
    this should have been part of commit a783d8a0226bb56263e5c678c41f2c6e53819ecf
    
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,37 @@
+#' @title DGLM-norm
+#'
+#' @param m.form mean formula
+#' @param d.form variance formula
+#' @param indata data
+#' @param maxiter max number of iterations to do
+#' @param conv converges when LL changes by less than x
+#'
+#' @return a list of length 3
+#' @export
+#'
+DGLM_norm <- function(m.form, d.form, data, maxiter=20, conv=1e-6) {
+  X.mean <- model.matrix(m.form, data = data)
+  X.disp <- model.matrix(d.form, data = data)
+  y.name <- all.vars(m.form)[1]
+  y <- data[[y.name]]
+  w <- rep(1, nrow(data))
+  convergence <- 1
+  iter <- 0
+  while (convergence > conv & iter < maxiter) {
+    iter <- iter + 1
+    w.old <- w
+    glm1 <- lm(y~.-1, weights = w, data=data.frame(X.mean))
+    res <- resid(glm1)
+    q <- hatvalues(glm1)
+    y2 <- res^2
+    glm2 <- glm(y2~.-1, family = Gamma(link = log), weights = (1 - q)/2, data = data.frame(X.disp))
+    w <- 1/fitted(glm2)
+    convergence <- (max(abs(w.old - w)) + (summary(glm1)$sigma - 1) )
+  }
+  return(list(mean = glm1, disp = glm2, iter = iter))
+}
+
+
 #' @title tryNA
 #' @description silently get what you can out of an expression
 #' @rdname internals
